15f9a799740166a0be35dda027cae2d0445d5b36
PRASAD MADHURANGA <102537177+prasad-kmd@users.noreply.github.com>
2025-02-23 00:59:19 +0530

    Add Tailwind CSS utilities and update related dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./_includes/**/*.html",
-      "./_layouts/**/*.html",
-      "./_posts/*.*",
-      "./*.html",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.*",
+    "./*.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#1da1f2', // Example custom color
+      },
+      fontFamily: {
+        'display': ['Oswald', 'sans-serif'], // Example custom font family
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
